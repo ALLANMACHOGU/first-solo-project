292f2e871c38bcfb28b88c20b469ce18ed97a300
ALLANMACHOGU <114505966+ALLANMACHOGU@users.noreply.github.com>
2025-07-08 23:08:10 +0000

    building from scratch

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  vite: {
+    build: {
+      minify: false,
+    },
+  },
+});
+
+// This configuration file is for an Astro project. AI typing this, I'm coping it from APCSP framework.
