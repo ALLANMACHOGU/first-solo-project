7ebaebcecfe815f703dca0d71cdb4455200ac5e0
ALLANMACHOGU <114505966+ALLANMACHOGU@users.noreply.github.com>
2025-08-20 22:36:43 +0000

    same old, but new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// filepath: /workspaces/apcsp-portfolio-ALLANMACHOGU/astro.config.mjs
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  base: process.env.BASE_PATH || "/",
   vite: {
     build: {
       minify: false,
